b84cd17d6fd008de69730faa13e2ea4f0908b5db
What42Pizza <burg.what42@gmail.com>
2026-03-19 20:41:50 -0500

    start work on spage-age-to-1.1-tweaks

---
+++ space-age-to-1.1-tweaks/data-updates.lua
@@ -0,0 +1,184 @@
+local utils = require("utils")
+
+
+
+-- foundry tech
+local foundry_tech = data.raw["technology"]["foundry"]
+remove_effect(foundry_tech, function(effect) return effect.recipe == "molten-iron-from-lava" end, "recipe \"molten-iron-from-lava\"")
+remove_effect(foundry_tech, function(effect) return effect.recipe == "molten-copper-from-lava" end, "recipe \"molten-copper-from-lava\"")
+remove_effect(foundry_tech, function(effect) return effect.recipe == "molten-iron" end, "recipe \"molten-iron\"")
+remove_effect(foundry_tech, function(effect) return effect.recipe == "molten-copper" end, "recipe \"molten-copper\"")
+
+
+
+-- replace all molten metal in recipes with 0.1x plate
+
+for _,recipe in pairs(data.raw["recipe"]) do
+	if recipe.ingredients then
+		for _,ingredient in ipairs(recipe.ingredients) do
+			if ingredient.name then
+				if ingredient.name == "molten-iron" then
+					ingredient.name = "iron-plate"
+					ingredient.amount = math.ceil(ingredient.amount / 10.0)
+					ingredient.type = "item"
+				end
+				if ingredient.name == "molten-copper" then
+					ingredient.name = "copper-plate"
+					ingredient.amount = math.ceil(ingredient.amount / 10.0)
+					ingredient.type = "item"
+				end
+			else
+				if ingredient[1] == "molten-iron" then
+					ingredient[1] = "iron-plate"
+					ingredient[2] = math.ceil(ingredient[2] / 10.0)
+				end
+				if ingredient[1] == "molten-copper" then
+					ingredient[1] = "copper-plate"
+					ingredient[2] = math.ceil(ingredient[2] / 10.0)
+				end
+			end
+		end
+	end
+end
+
+-- remove all molten metals
+data.raw["fluid"]["molten-iron"].hidden = true
+data.raw["fluid"]["molten-copper"].hidden = true
+
+
+
+-- casting iron recipe
+local casting_iron_recipe = data.raw["recipe"]["casting-iron"]
+casting_iron_recipe.ingredients = {
+	{
+		amount = 10,
+		name = "iron-ore",
+		type = "item"
+	},
+	{
+		amount = 1,
+		name = "calcite",
+		type = "item"
+	}
+}
+casting_iron_recipe.results = {
+	{
+		amount = 10,
+		name = "iron-plate",
+		type = "item"
+	}
+}
+casting_iron_recipe.energy_required = 32.0
+
+-- casting copper recipe
+local casting_copper_recipe = data.raw["recipe"]["casting-copper"]
+casting_copper_recipe.ingredients = {
+	{
+		amount = 10,
+		name = "copper-ore",
+		type = "item"
+	},
+	{
+		amount = 1,
+		name = "calcite",
+		type = "item"
+	}
+}
+casting_copper_recipe.results = {
+	{
+		amount = 10,
+		name = "copper-plate",
+		type = "item"
+	}
+}
+casting_copper_recipe.energy_required = 32.0
+
+
+
+
+
+-- foundries
+local foundry = data.raw["assembling-machine"]["foundry"]
+foundry.crafting_speed = 2
+foundry.effect_receiver.base_effect.productivity = 0.25
+foundry.energy_usage = "750kW"
+
+-- biochamber
+local biochamber = data.raw["assembling-machine"]["biochamber"]
+biochamber.effect_receiver.base_effect.productivity = 0.25
+
+-- electromagnetic plant
+local electromagnetic_plant = data.raw["assembling-machine"]["electromagnetic-plant"]
+electromagnetic_plant.crafting_speed = 1.5
+electromagnetic_plant.effect_receiver.base_effect.productivity = 0.25
+electromagnetic_plant.energy_usage = "750kW"
+
+-- biolab
+local biolab = data.raw["lab"]["biolab"]
+biolab.researching_speed = 1.5
+biolab.science_pack_drain_rate_percent = 75
+
+
+
+
+
+-- inserters
+function update_bulk_inserter_size_increase(tech)
+	for _,effect in ipairs(tech.effects) do
+		if effect.type == "bulk-inserter-capacity-bonus" then
+			effect.modifier = 1
+		end
+	end
+end
+update_bulk_inserter_size_increase(data.raw["technology"]["inserter-capacity-bonus-5"])
+update_bulk_inserter_size_increase(data.raw["technology"]["inserter-capacity-bonus-6"])
+update_bulk_inserter_size_increase(data.raw["technology"]["inserter-capacity-bonus-7"])
+remove_effect(data.raw["technology"]["inserter-capacity-bonus-7"], function(effect) return effect.type == "inserter-stack-size-bonus" end)
+local stack_inserter = data.raw["inserter"]["stack-inserter"]
+stack_inserter.stack_size_bonus = 3
+stack_inserter.max_belt_stack_size = 3
+
+
+
+
+
+-- asteroid reprocessing
+local carbonic_asteroid_reprocessing = data.raw["recipe"]["carbonic-asteroid-reprocessing"]
+local metallic_asteroid_reprocessing = data.raw["recipe"]["metallic-asteroid-reprocessing"]
+local oxide_asteroid_reprocessing    = data.raw["recipe"]["oxide-asteroid-reprocessing"]
+for _,recipe in ipairs({carbonic_asteroid_reprocessing, metallic_asteroid_reprocessing, oxide_asteroid_reprocessing}) do
+	for _,result in ipairs(recipe.results) do
+		result.probability = result.probability * 0.625
+	end
+end
+
+
+
+
+
+-- quality level tweaks
+data.raw["quality"]["uncommon"] .beacon_power_usage_multiplier = nil
+data.raw["quality"]["rare"]     .beacon_power_usage_multiplier = nil
+data.raw["quality"]["epic"]     .beacon_power_usage_multiplier = nil
+data.raw["quality"]["legendary"].beacon_power_usage_multiplier = nil
+
+-- quality modules
+local quality_module_1 = data.raw["module"]["quality-module"]
+local quality_module_2 = data.raw["module"]["quality-module-2"]
+local quality_module_3 = data.raw["module"]["quality-module-3"]
+
+-- speed module 3
+local speed_module_3 = data.raw["module"]["speed-module-3"]
+speed_module_3.effect.quality = -0.2
+
+-- beacons
+local beacon = data.raw["beacon"]["beacon"]
+beacon.distribution_effectivity_bonus_per_quality_level = 0.0
+
+-- chain tesla gun chain
+local chain_tesla_gun_chain = data.raw["chain-active-trigger"]["chain-tesla-gun-chain"]
+chain_tesla_gun_chain.fork_chance_increase_per_quality_level = 0.025
+
+-- chain tesla turret chain
+local chain_tesla_turret_chain = data.raw["chain-active-trigger"]["chain-tesla-turret-chain"]
+chain_tesla_turret_chain.fork_chance_increase_per_quality_level = 0.025
---
+++ space-age-to-1.1-tweaks/readme.md
@@ -0,0 +1,17 @@
+# Space Age To 1.1 Tweaks (Factorio Mod)
+
+![example](example.png)
+
+## WARNING: This mod has little testing and likely still needs some work. If you have any suggestions or ideas then please let me know, I might not implement it but I'd still love to hear it!
+
+Contains a collection of tweaks that aim to make Space Age bases more similar to pre- Space Age bases in a few select ways. I plan for this to fit into a larger overhaul-like mod that I'll hopefully make soon, and until then, this mod will be somewhat untested.
+
+## Changes:
+
+- Molten iron and copper no longer exist (I want rivers of iron, not a single pipe that teleports generic iron units)
+- Foundries, elecro-magnetic plants, and biochambers now have a base productivity of 25% (I want endless rows of machines, not a few very powerful ones)
+- Foundries and electro-magnetic plants have reduced crafting speed and energy consumption (again, I want endless rows of machines)
+- Stack inserters now hold 12 items max and drop stacks of 3 items
+- Bulk inserters now hold 9 items max (because stack inserters are the new bulk inserters)
+- All other inserters hold 3 items max
+- Asteroid reprocessing now returns 50% instead of 80% (because space casinos are too powerful & this now acts as a good way to void asteroids)
